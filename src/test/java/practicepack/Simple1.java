2ee99ad2edde438ef034094fa026f4375124562c
Divyak21998 <divyak21998@email.com>
2023-12-03 10:23:07 +0530

    gf

--- src/test/java/practicepack/Simple1.java
+++ src/test/java/practicepack/Simple1.java
@@ -5,9 +5,7 @@ import org.testng.annotations.Test;
 public class Simple1 {
 	@Test
 	public void demo() {
-		System.out.println("----demo1----");
-		System.out.println("-----demo2----");
-		System.out.println("---demo3----");
+		
 	}
 
 }
