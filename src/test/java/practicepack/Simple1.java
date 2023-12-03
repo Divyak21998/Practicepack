826c5badd27ae0ee92a48779383547ce9c9ff8c2
Divyak21998 <divyak21998@email.com>
2023-12-03 10:08:41 +0530

    1c

---
+++ src/test/java/practicepack/Simple1.java
@@ -0,0 +1,12 @@
+package practicepack;
+
+import org.testng.annotations.Test;
+
+public class Simple1 {
+	@Test
+	public void demo() {
+		System.out.println("----demo1----");
+		System.out.println("-----demo2----");
+	}
+
+}
